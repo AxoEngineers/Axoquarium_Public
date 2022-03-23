cb3da3b17fde0113fdc3f81d69139aada3be8d5f
Ori Wagmi <ori.wagmi@gmail.com>
2022-03-22 23:23:25 -0700

    add server

---
+++ axolittles-master-server/axo.js
@@ -0,0 +1,119 @@
+/*
+==========================================================
+  ___ _   _ _____ _____ _____ ____      _    ____ _____ 
+ |_ _| \ | |  ___|_   _| ____|  _ \    / \  / ___|_   _|
+  | ||  \| | |_    | | |  _| | |_) |  / _ \| |     | |  
+  | || |\  |  _|   | | | |___|  _ <  / ___ \ |___  | |  
+ |___|_| \_|_|     |_| |_____|_| \_\/_/   \_\____| |_|  
+ 
+==========================================================
+
+							NOTES
+
+ (*) Author: Aleks                                       					
+					
+==========================================================
+*/
+
+// native dependencies
+const readline = require("readline");
+const path = require('path');
+
+// our depdendencies
+const api = require('./main');
+const config = require('./config');
+const cm = require('./contractmanager');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const express = require("express");
+const cors = require('cors');
+const app = express();
+app.use(cors());
+const router = express.Router();
+
+if (config.env != "local") 
+{
+	const fs = require('fs');
+	let references = fs.readFileSync('./game/StreamingAssets/aa/catalog.json', 'utf8');
+	references = references.replace(new RegExp("http://localhost:3000", 'g'), "https://axoquarium.herokuapp.com");
+	fs.writeFileSync('game/StreamingAssets/aa/catalog.json', references);
+	console.log("Rewriting localhost references to catalog according to environment");
+}
+
+router.use('/', express.static('game'));
+router.use('/WebGL', express.static('assetbundles'));
+router.use('/avatar', express.static('av-fetcher/images'));
+
+/// OUR OWN REST API (REPLACES MORALIS WOO!!)
+/*router.get("/owned/:contract/:addr", async(req, res, next) => {
+	if (req.params.addr == "filter") {
+		res.send(await cm.getOwnedByFilter(req.params.contract, req.query))
+	} else {
+		res.send(await cm.getOwnedNfts(req.params.contract, req.params.addr))
+	}
+})*/
+
+router.get("/all/:addr", async(req, res, next) => {
+	res.send(await cm.getAllOwnedNfts(req.params.addr));
+})
+
+/*router.get("/staked/:contract/:addr", async(req, res, next) => {
+	res.send(await cm.getStakedNfts(req.params.contract, req.params.addr))
+})
+
+router.get("/unstaked/:contract/:addr", async(req, res, next) => {
+	res.send(await cm.getUnstakedNfts(req.params.contract, req.params.addr))
+})*/
+
+// Initialization Section
+function promptInput() {
+	rl.question("", function(str) {
+		
+		let cmd = str;
+		let prefix = "/";
+		
+		if (cmd.startsWith(prefix))
+		{
+			cmd = cmd.substring(1);
+		}
+		
+		if ((/[a-zA-Z]/).test(cmd.charAt(0)))
+		{
+			const args = cmd.trim().split(' ');
+			const command = args.shift().toLowerCase();
+		
+			if (command == "exit") {
+				process.exit(1);
+			}
+		}
+		
+		promptInput();
+	});
+}
+
+(async () => {
+	app.use('/', router);
+	app.listen(config.port, () => {
+		console.log(`Server running on port ${config.port}`);
+	});
+	
+	// Pixelverse Item (PVIT)
+	await cm.createStaticContracts()
+	
+	let handler = undefined
+	
+	// SEALS
+	console.log("LOADING AXO NORMAL/STAKE CONTRACT ...")
+	// 0xf36 is the Axolittles contract. 
+	// 0x1ca6 is the original staking contract.
+	// 0xbfcA is the V2 staking contract.
+	handler = await cm.registerContractLedger("0xf36446105fF682999a442b003f2224BcB3D82067", 13171333, "0x1ca6e4643062e67ccd555fb4f64bee603340e0ea", "0xbfcA4318f4d47f8A8e49e16c0f2B466c46EAC184")
+	await handler.fetch()
+	
+})();
+
+promptInput();
